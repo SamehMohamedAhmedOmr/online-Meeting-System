d5dd7a1e3c320fb58610aa925b92073e29e479bb
MeetingSystem <medoaboserii@gmail.com>
2019-05-22 14:50:18 +0200

    translation package uploaded

---
+++ resources/lang/ar/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'Users' => 'الأشخاص',
+    'faculties' => 'الكليات',
+    'Departments' => 'الاقسام',
+    'Council Definitions' => 'مجالس الاقسام',
+    'Image'=>'الصوره',
+    'options'=>'العمليات',
+    'Name'=>'الاسم',
+    'Email'=>'العنوان البريدي',
+    'Position'=>'المنصب',
+    'Show '=>'عرض',
+    'entries '=>'صفوف',
+
+
+
+];
---
+++ vendor/joedixon/laravel-translation/database/migrations/2018_08_29_200844_create_languages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use JoeDixon\Translation\Language;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('translation.database.languages_table'), function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('language');
+            $table->timestamps();
+        });
+
+        Language::create([
+            'language' => config('app.locale'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('translation.database.languages_table'));
+    }
+}
